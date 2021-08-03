b593d85a86813389d15edfc96bf3f7a993598c57
senamegh <greghount@yahoo.fr>
2021-08-03 23:14:03 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    document.getElementById(interest).value;
+    var year = new Date().getFullYear()+parseInt(year);
+    document.getElementById("result").innerHTML="if you deposit"(principal)/ <br/>
+    "at an interest rate of"(rate)%/ <br/> "you will receive an amount of"(amount)/ <br/> in the (year)/ <br/>
+    "No of years".value=="year"
 }
-        
+function updateRate()
+{
+    var rate_val = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rate_val
+}       
+function checkdata()
+{
+    var principal = document.getElementById(principal);
+    if(principal.value=="0") {
+        alert("enter a positive value")
+        principal.focus(); return principal;
+    }
+    if(principal.value==""){
+        alert("enter a positive value")
+        principal.focus(); return principal;
+    }
+}
