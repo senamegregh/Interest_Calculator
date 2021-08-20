cd683cd041c595160352175bb2a030810dbfab85
senamegh <greghount@yahoo.fr>
2021-08-20 18:05:41 -0400

    changes made from visual studio code

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
+var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    document.getElementById(interest).value;
-    var year = new Date().getFullYear()+parseInt(year);
-    document.getElementById("result").innerHTML="if you deposit"(principal)/ <br/>
-    "at an interest rate of"(rate)%/ <br/> "you will receive an amount of"(amount)/ <br/> in the (year)/ <br/>
-    "No of years".value=="year"
-}
-function updateRate()
-{
-    var rate_val = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rate_val
+    var year = new Date().getFullYear()+parseInt(years);
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rateval").innerText=rateval;
 }       
-function checkdata()
-{
-    var principal = document.getElementById(principal);
+updateRate("rateval");
+
+var result = document.getElementById("interest").value;
+
+function compute(){
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
     if(principal.value=="0") {
         alert("enter a positive value")
         principal.focus(); return principal;
@@ -26,4 +21,4 @@ function checkdata()
         alert("enter a positive value")
         principal.focus(); return principal;
     }
-}
+}onclick.button=compute();
