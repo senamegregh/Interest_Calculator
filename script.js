d399dd2decf5b6688fc95e6014995e5dd064994e
senamegregh <greghount@yahoo.fr>
2021-09-02 07:16:15 -0400

    changes made from visual studio

--- script.js
+++ script.js
@@ -2,20 +2,12 @@ function updateRate(){
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }       
-updateRate("rateval");
 function compute() { 
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>, <br\>at an interest rate of <mark>"+rate+"%\</mark>.<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark> \<br\>" }
-compute();
-function validateAmount() { var principal = document.getElementById("principal").value;
-if(principal<="0") {
-        alert("Enter a positive value");
-        document.getElementById("principal").focus(); return principal;
-    }
+if(principal ==undefined || principal <=0){ alert("enter a positive number")} else {(document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>, <br\>at an interest rate of <mark>"+rate+"%\</mark>.<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark> \<br\>")}
 }
-validateAmount();
-     
+compute();
